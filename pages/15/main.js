153d2cc41b1896c7444ac7fc542b034771853902
Fernanda Dias <fernandadias.dev@gmail.com>
2023-06-05 11:10:05 -0300

    feat: :sparkles: adiciona exercícios

---
+++ pages/15/main.js
@@ -0,0 +1,16 @@
+const valor1 = 2;
+const valor2 = 7;
+
+const soma = valor1 + valor2;
+const divisao = valor1 / valor2;
+const multiplicacao = valor1 * valor2;
+const igual = valor1 === valor2;
+const maior = valor1 > valor2;
+const menor = valor1 < valor2;
+
+console.log("Soma: " + soma);
+console.log("Divisão: " + divisao);
+console.log("Multiplicação: " + multiplicacao);
+console.log("Igual: " + igual);
+console.log("Maior: " + maior);
+console.log("Menor: " + menor);
